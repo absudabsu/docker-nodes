26617fcfd14d2f8617ce6dafc1ead95d1d4c90fe
Abhejit Rajagopal <rajagopal.abhejit@gmail.com>
2017-08-16 14:58:24 -0700

    explicit python path

--- tf-cpu.sh
+++ tf-cpu.sh
@@ -17,15 +17,21 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     exit 0
 fi
 echo '--> '$platform
+echo 'pwd='$(pwd)
+ORIG_DIR=$(pwd)
+echo $(which python3)
+echo $(which python)
+echo $(which pip)
 ## -----------------------------------------------------------------------------
 
 ## Build from scratch baby
 # CPU-src: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tools/docker/Dockerfile.devel
 ## Set up Bazel.
+cd $ORIG_DIR
 echo "startup --batch" >> /etc/bazel.bazelrc
 echo "build --spawn_strategy=standalone --genrule_strategy=standalone" >> /etc/bazel.bazelrc
 export BAZEL_VERSION=0.5.0
-export BAZEL_DIR=$(pwd)/bazel
+export BAZEL_DIR="$(pwd)"/bazel
 mkdir -p $BAZEL_DIR && \
     cd $BAZEL_DIR && \
     curl -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36" -fSsL -O https://github.com/bazelbuild/bazel/releases/download/$BAZEL_VERSION/bazel-$BAZEL_VERSION-installer-linux-x86_64.sh && \
@@ -36,12 +42,13 @@ mkdir -p $BAZEL_DIR && \
     rm -f $BAZEL_DIR/bazel-$BAZEL_VERSION-installer-linux-x86_64.sh
 
 # Download and build TensorFlow.
+cd $ORIG_DIR
 export TF_DIR=$(pwd)/tensorflow
 git clone https://github.com/tensorflow/tensorflow.git $TF_DIR && \
     cd $TF_DIR && \
     git checkout r1.3
 cd $TF_DIR
-export CI_BUILD_PYTHON=python
+export CI_BUILD_PYTHON=/opt/intel/intelpython35/bin//python3
 tensorflow/tools/ci_build/builds/configured CPU \
     bazel build -c opt --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0" \
         tensorflow/tools/pip_package:build_pip_package && \
